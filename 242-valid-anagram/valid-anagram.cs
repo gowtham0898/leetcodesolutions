210dadb898311553d1631a19bccbfc43fce86ecd
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-06-05 16:37:40 +0530

    Time: 143 ms (5.08%) | Memory: 42.1 MB (62.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,10 +1,31 @@
 public class Solution {
-    public bool IsAnagram(string s, string t) {
-        
-        var sorce = s.ToCharArray();
-        var target = t.ToCharArray();
-       Array.Sort(sorce);
-       Array.Sort(target);
-       return sorce.SequenceEqual(target);
-    }
+    public bool IsAnagram(string s, string t)
+    {
+     
+     if (s.Length != t.Length)
+     {
+         return false;
+     }
+     List<char> source = new List<char>(s);
+     List<char> target = new List<char>(t);
+     for ( int i = 0; i < source.Count(); i++)
+     {
+         if (target.Contains(source[i]))
+         {
+             target.Remove(source[i]);
+         }
+     }
+     if(target.Count != 0) 
+     {
+         return false;
+     }
+     return true;
+
+    // this solution with out using sort
+    //  var sorce = s.ToCharArray();
+        // var target = t.ToCharArray();
+    //    Array.Sort(sorce);
+    //    Array.Sort(target);
+    //    return sorce.SequenceEqual(target);
+ }
 }
