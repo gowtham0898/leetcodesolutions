0d1b640e9887e64e53eeace12d212ff5a80db990
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-06-09 01:19:41 +0530

    Time: 107 ms (6.32%) | Memory: 38.4 MB (93.68%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        IList<IList<int>> result = new List<IList<int>> ();
+
+        for(int c = 0; c < numRows; c++){
+            IList<int> row = new List<int>(new int[c+1]);
+            row[0] = 1; // 1st index
+            row[row.Count - 1] = 1; // last index
+            for( int r = 1; r < c; r++){ // exclude 1st and last column
+            row[r] = result[c-1][r-1] + result[c-1][r];
+            }
+            result.Add(row);
+        }
+        return result;
+    }
+}
