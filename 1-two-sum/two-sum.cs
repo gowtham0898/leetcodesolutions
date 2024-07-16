e00a3cb75942ee7a2642f8ea5d56753adb9552a9
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-16 21:11:18 +0530

    Time: 116 ms (77.25%) | Memory: 47.7 MB (48.97%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -3,15 +3,18 @@ public class Solution {
         
         for(int i = 0; i < nums.Length ; i++){
             var _target = target - nums[i];
+
             if(nums.Contains(_target)){
+                // using Array bcoz IndexOf function
                 var _index = Array.IndexOf(nums,_target);
+                 // should not be same index so 
                 if(_index  != i)
                 {
                     return new int[]{_index,i};
-             }
                 }
-                
+            }   
         }
+        // return an empty array, but size should be defined 
         return new int[0];
     }
 }
