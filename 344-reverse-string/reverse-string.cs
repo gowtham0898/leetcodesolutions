2ba56d0a6e8a7107e9e4f49b3dcfc2271c2721c8
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-18 00:28:28 +0530

    Time: 233 ms (34.25%) | Memory: 129.5 MB (20.90%) - LeetSync

--- 344-reverse-string/reverse-string.cs
+++ 344-reverse-string/reverse-string.cs
@@ -1,13 +1,12 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        int i = 0;
-        int n = s.Length -1 ;
-
-      while(i < n){
-        char temp = s[i]; // store in temp variable
-        s[i] = s[n]; // assign  to the last index
-        s[n] = temp; 
-        i ++ ; n--;
-      }
+        int l = 0;
+        int n = s.Length -1;
+         while(l < n){
+            s[l] ^= s[n];
+            s[n] ^= s[l];
+            s[l] ^= s[n];
+            l++; n--;
+         }
     }
 }
