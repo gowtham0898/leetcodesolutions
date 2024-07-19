26ad7906300424692015f4db2cfcda644146007c
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-19 07:44:39 +0530

    Time: 50 ms (49.98%) | Memory: 33.5 MB (36.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.cs
+++ 9-palindrome-number/palindrome-number.cs
@@ -2,13 +2,23 @@ public class Solution {
     public bool IsPalindrome(int x) {
         var string_number = x.ToString();
 
-  char [] chars = string_number.ToCharArray();
-  Array.Reverse(chars);
-  var reverese_char = new string(chars);
-  if(string_number == reverese_char)
-  {
-      return true;
-  }
-  return false;
+         char [] chars = string_number.ToCharArray();
+        // Array.Reverse(chars);
+        // var reverese_char = new string(chars);
+        // if(string_number == reverese_char)
+        // {
+        //     return true;
+        // }
+        // return false;
+
+        int mid = 0;
+        int last= chars.Length -1 ;
+        while (mid < last){
+            if(chars[mid] != chars[last]){
+                return false;
+            }
+            mid++;last--;
+        }
+        return true;
     }
 }
