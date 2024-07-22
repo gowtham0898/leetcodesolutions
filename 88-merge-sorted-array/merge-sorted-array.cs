0111ca7a66541a9d60a63efde8c1f54e9d7d3631
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-22 23:19:49 +0530

    Time: 108 ms (26.94%) | Memory: 45.9 MB (53.46%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+    int j = 0;
+    for(int i = 0; i < nums1.Length; i++){
+        if(nums1[i]==0)
+        {
+            if(n > j){
+            nums1[i] = nums2[j];
+            
+                j++;
+            }
+        }
+    }
+    Array.Sort(nums1); // nlogn
+
+    }
+}
