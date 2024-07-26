17bc4a77724659ffd32998710a1d1a623f7e585d
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-26 12:36:17 +0530

    Time: 216 ms (83.35%) | Memory: 71.8 MB (22.35%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -1,55 +1,58 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
         
-        // HashSet<int> unique = new HashSet<int>();
-        // foreach (var num in nums)
-        // {
-        //     unique.Add(num);
-        // }
-        // int max_count = 0;
-        // foreach (var unique_ in unique)
-        // {
-
-        //     if (!unique.Contains(unique_ - 1))
-        //     {
-        //         int count = 1;
-        //         int x = unique_+1;
-        //         while (unique.Contains(x))
-        //         {
-        //             count++;
-        //             x++;
-        //         }
-        //         if (count > max_count)
-        //         {
-        //             max_count = count;
-        //         }
-        //     }
-        // }
-        //  return max_count;
-        int max_count = 0;
-        Array.Sort(nums);
-
-        var count = 1;
-        int x = int.MinValue;
-        for (int i = 0; i < nums.Length; i++)
+        HashSet<int> unique = new HashSet<int>();
+        foreach (var num in nums)
         {
-        
-            var value = nums[i];
-            if (nums.Contains(value + 1) && value != x)
-            {
-                count++;
-                x = nums[i];
-            }
-            else if(!nums.Contains(value + 1))
-            {
-                count = 1;
-            }
+            unique.Add(num);
+        }
+        int max_count = 0;
+        foreach (var unique_ in unique)
+        {
 
-            if(count > max_count)
+            if (!unique.Contains(unique_ - 1))
             {
-                max_count = count;
+                int count = 1;
+                int x = unique_+1;
+                while (unique.Contains(x))
+                {
+                    count++;
+                    x++;
+                }
+                if (count > max_count)
+                {
+                    max_count = count;
+                }
             }
         }
-        return max_count;
+         return max_count;
+        // solution with sort function
+        // int max_count = 0;
+        // Array.Sort(nums);
+
+        // var count = 1;
+        // int x = int.MinValue;
+        // for (int i = 0; i < nums.Length; i++)
+        // {
+        
+        //     var value = nums[i];
+            // check for next number is there and not same value
+        //     if (nums.Contains(value + 1) && value != x)
+        //     {
+        //         count++;
+        //         x = nums[i];
+        //     }
+        //if not then set the pointer to zero
+        //     else if(!nums.Contains(value + 1))
+        //     {
+        //         count = 1;
+        //     }
+
+        //     if(count > max_count)
+        //     {
+        //         max_count = count;
+        //     }
+        // }
+        // return max_count;
     }
 }
