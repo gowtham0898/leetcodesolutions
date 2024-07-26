44c9963b1290c8478b5294c928f630a3499cdcc9
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-07-26 18:18:16 +0530

    Time: 108 ms (29.68%) | Memory: 45.3 MB (96.79%) - LeetSync

--- 48-rotate-image/rotate-image.cs
+++ 48-rotate-image/rotate-image.cs
@@ -1,24 +1,46 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
-        int[][] copy_matrix = new int[matrix.Length][];
-        int n = matrix.Length;
-        for (int i = 0; i < matrix.Length; i++)
-        {
-            copy_matrix[i] = new int[matrix[i].Length];
-        }
-        for(int i = 0; i < matrix.Length; i++)
-        {
-            for(int j = 0; j < matrix[i].Length; j++)
+
+         int n = matrix.Length;
+            for (int i = 0; i < n; i++)
             {
-                copy_matrix[j][n - 1 - i] = matrix[i][j];
+                for (int j = i; j < n; j++)
+                {
+                    var temp = matrix[i][j];                
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
             }
-        }
-        for(int i = 0;i < matrix.Length; i++)
-        {
-            for(int j = 0;j < matrix[i].Length; j++)
+
+            for (int i = 0;i < n; i++)
             {
-                matrix[i][j] = copy_matrix[i][j];
+                for(int j = 0; j < n/2;j++)
+                {
+                    var temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n - 1 - j];
+                    matrix[i][n - 1 - j] = temp;
+
+                }
             }
-        }
+        // int[][] copy_matrix = new int[matrix.Length][];
+        // int n = matrix.Length;
+        // for (int i = 0; i < matrix.Length; i++)
+        // {
+        //     copy_matrix[i] = new int[matrix[i].Length];
+        // }
+        // for(int i = 0; i < matrix.Length; i++)
+        // {
+        //     for(int j = 0; j < matrix[i].Length; j++)
+        //     {
+        //         copy_matrix[j][n - 1 - i] = matrix[i][j];
+        //     }
+        // }
+        // for(int i = 0;i < matrix.Length; i++)
+        // {
+        //     for(int j = 0;j < matrix[i].Length; j++)
+        //     {
+        //         matrix[i][j] = copy_matrix[i][j];
+        //     }
+        // }
     }
 }
