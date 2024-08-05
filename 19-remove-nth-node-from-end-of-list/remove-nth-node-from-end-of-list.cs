3279f23da369ef872f7a83a1193fd810f616166b
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2024-08-05 12:25:05 +0530

    Time: 53 ms (92.19%) | Memory: 41.1 MB (53.09%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cs
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cs
@@ -11,34 +11,52 @@
  */
 public class Solution {
     public ListNode RemoveNthFromEnd(ListNode head, int n) {
+
         if(head == null ){
             return null;
         }
+         ListNode fast = head;
+         for(int i =0; i< n; i++){
+            fast = fast.next;
+         }
+           // if we want to delete head only 
+         if(fast == null){
+            return head.next;
+         }
+         ListNode slow = head;
+         while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+         }
+          
+         slow.next = slow.next.next;
+         return head;
 
-        int count = 0;
-        ListNode current = head;
-        while(current != null){
-            current = current.next; 
-            count++;
-        }
-        if(count ==1){
-            return null;
-        }
-      int toDeleteNode = count - n;
 
-      int currentCount = 1;
-        if(toDeleteNode ==0){
-            head = head.next;
-        }
-      ListNode temp = head;
-      while(temp != null){
+    //     int count = 0;
+    //     ListNode current = head;
+    //     while(current != null){
+    //         current = current.next; 
+    //         count++;
+    //     }
+    //     if(count ==1){
+    //         return null;
+    //     }
+    //   int toDeleteNode = count - n;
+
+    //   int currentCount = 1;
+    //     if(toDeleteNode ==0){
+    //         head = head.next;
+    //     }
+    //   ListNode temp = head;
+    //   while(temp != null){
        
-        if(currentCount == toDeleteNode){
-            temp.next = temp.next.next;
-        }
-        temp = temp.next;
-        currentCount++;
-      }
-      return head;
+    //     if(currentCount == toDeleteNode){
+    //         temp.next = temp.next.next;
+    //     }
+    //     temp = temp.next;
+    //     currentCount++;
+    //   }
+    //   return head;
     }
 }
