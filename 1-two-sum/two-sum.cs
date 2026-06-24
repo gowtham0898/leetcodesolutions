b2aae064cbefa569df53a3cc1611aab87420ea37
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-06-24 20:58:44 +0530

    Time: 4 ms (88.55%) | Memory: 50.1 MB (22.13%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,20 +1,14 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
-        for(int i = 0; i < nums.Length ; i++){
-            var _target = target - nums[i];
+        Dictionary<int,int> map = new();
 
-            if(nums.Contains(_target)){
-                // using Array bcoz IndexOf function
-                var _index = Array.IndexOf(nums,_target);
-                 // should not be same index so 
-                if(_index  != i)
-                {
-                    return new int[]{_index,i};
-                }
-            }   
+        for(int i = 0; i< nums.Length; i++){
+            int rem = target - nums[i];
+            if(map.ContainsKey(rem)){
+                return new int[] {map[rem],i};
+            }
+            map[nums[i]] = i;
         }
-        // return an empty array, but size should be defined 
-        return new int[0];
+        return new int[]{0,0};
     }
 }
