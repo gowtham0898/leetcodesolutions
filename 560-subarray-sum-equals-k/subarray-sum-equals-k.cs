0946386e8f5033d205b619d72696c2a4aa45bf51
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-06-25 11:19:15 +0530

    Time: 17 ms (57.54%) | Memory: 53.8 MB (12.29%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+     int cr_sum = 0;
+     int result = 0;
+     Dictionary<int,int> map = new();
+     map[0] = 1;
+
+     for(int i = 0; i < nums.Length; i++){
+        cr_sum += nums[i];
+        int pre_sum = cr_sum - k;
+        if(map.ContainsKey(pre_sum)){
+            result += map[pre_sum];
+        }
+
+        if(map.ContainsKey(cr_sum)){
+            map[cr_sum]++;
+        }
+        else{
+            map[cr_sum] = 1;
+        }
+     }
+     return result;
+    }
+}
