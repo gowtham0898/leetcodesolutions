4081b9e50eb89158bb23d1e15a8b2985b005fff6
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-06-25 07:04:58 +0530

    Time: 14 ms (40.66%) | Memory: 48.6 MB (22.10%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        if(ransomNote.Length > magazine.Length){
+            return false;
+        }
+        Dictionary<char, int> map = new();
+
+        for(int i =0; i<magazine.Length;i++){
+            if(map.ContainsKey(magazine[i])){
+               map[magazine[i]]++;
+            }
+            else{
+                map[magazine[i]] = 1;
+            }
+        }   
+
+        for(int i = 0; i<ransomNote.Length; i++){
+            if(map.ContainsKey(ransomNote[i])){
+                map[ransomNote[i]]--;
+                if(map[ransomNote[i]] == 0){
+                    map.Remove(ransomNote[i]);
+                }                
+            }
+            else{
+                    return false;
+                }
+        }
+        return true;
+    }
+}
