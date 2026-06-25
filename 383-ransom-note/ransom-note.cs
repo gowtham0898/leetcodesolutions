8b0220ee3d0903a580fb247dccdbd6794638c0a8
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-06-25 07:16:57 +0530

    Time: 1 ms (99.02%) | Memory: 45.9 MB (86.57%) - LeetSync

--- 383-ransom-note/ransom-note.cs
+++ 383-ransom-note/ransom-note.cs
@@ -3,28 +3,40 @@ public class Solution {
         if(ransomNote.Length > magazine.Length){
             return false;
         }
-        Dictionary<char, int> map = new();
+        int[] num = new int[26];
 
         for(int i =0; i<magazine.Length;i++){
-            if(map.ContainsKey(magazine[i])){
-               map[magazine[i]]++;
-            }
-            else{
-                map[magazine[i]] = 1;
-            }
-        }   
-
+            num[magazine[i] - 'a'] ++;
+        }
         for(int i = 0; i<ransomNote.Length; i++){
-            if(map.ContainsKey(ransomNote[i])){
-                map[ransomNote[i]]--;
-                if(map[ransomNote[i]] == 0){
-                    map.Remove(ransomNote[i]);
-                }                
-            }
-            else{
-                    return false;
-                }
+             num[ransomNote[i] - 'a'] --;
+             if(num[ransomNote[i] - 'a'] < 0){
+                return false;
+             }
         }
         return true;
+        // Dictionary<char, int> map = new();
+
+        // for(int i =0; i<magazine.Length;i++){
+        //     if(map.ContainsKey(magazine[i])){
+        //        map[magazine[i]]++;
+        //     }
+        //     else{
+        //         map[magazine[i]] = 1;
+        //     }
+        // }   
+
+        // for(int i = 0; i<ransomNote.Length; i++){
+        //     if(map.ContainsKey(ransomNote[i])){
+        //         map[ransomNote[i]]--;
+        //         if(map[ransomNote[i]] == 0){
+        //             map.Remove(ransomNote[i]);
+        //         }                
+        //     }
+        //     else{
+        //             return false;
+        //         }
+        // }
+        // return true;
     }
 }
