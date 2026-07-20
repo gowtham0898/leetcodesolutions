92264837ca58c8244cfb14944b1e6808e7eb2be8
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-20 12:07:45 +0530

    Time: 11 ms (10.65%) | Memory: 49.9 MB (26.35%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cs
+++ 724-find-pivot-index/find-pivot-index.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
-        int total = nums.Sum();
-        int left_sum = 0;
-        for(int i = 0; i < nums.Length; i++){
-            int right_sum = total - nums[i]  - left_sum;
-            if(left_sum == right_sum){
-                return i;
-            }
-            left_sum += nums[i];
+       int total_sum = nums.Sum();
+       int left_sum = 0;
+       for(int i = 0; i < nums.Length; i++){
+        int right_sum = total_sum - left_sum - nums[i];
+        if(right_sum == left_sum){
+            return i;
         }
-        return -1;
+
+        left_sum += nums[i];
+       } 
+       return -1;
     }
 }
