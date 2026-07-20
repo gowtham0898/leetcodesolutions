366d93af5f86a23a6b041f9f27f92664e2dce5db
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-20 16:16:41 +0530

    Time: 15 ms (24.48%) | Memory: 49.5 MB (78.65%) - LeetSync

--- 525-contiguous-array/contiguous-array.cs
+++ 525-contiguous-array/contiguous-array.cs
@@ -3,6 +3,7 @@ public class Solution {
         int one = 0;
         int zero = 0;
         Dictionary<int, int> map = new();
+        map[0] = -1;
         int result = 0;
         for(int i = 0; i < nums.Length; i++){
             if(nums[i] == 0){
@@ -11,18 +12,14 @@ public class Solution {
             else{
                 one +=1;
             }
-
             int dif = one - zero;
-            if(!map.ContainsKey(dif)){
-                map[dif] = i;
-            }
-            if(one == zero){
-                result = one + zero;
-            }
-            else{
+            if(map.ContainsKey(dif)){
                 int dif_idx = map[dif];
                result =  Math.Max(result, i - dif_idx);
-            }            
+            }
+            else{
+                 map[dif] = i;
+            }                              
         } 
         return result;
     }
