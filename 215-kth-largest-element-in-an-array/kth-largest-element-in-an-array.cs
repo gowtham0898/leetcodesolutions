212465972333a9a2cf66fb311ee86458a9f9bf7f
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-21 21:18:08 +0530

    Time: 2028 ms (5.12%) | Memory: 60.9 MB (37.21%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
@@ -1,30 +1,73 @@
 public class Solution {
-    public int FindKthLargest(int[] nums, int k) {       
-       k = nums.Length -k;
+    public int FindKthLargest(int[] nums, int k) { 
+        k = nums.Length - k;
 
-        return QuickSelect(nums, 0, nums.Length -1, k);
+        return QuickSelect(nums, 0, nums.Length - 1, k);
     }
-    private int QuickSelect(int[] nums, int l,int r,int k){
-        if(l == r){
-            return nums[l];
+
+    private int QuickSelect(int[] nums, int left, int right, int result_index){
+        int _base = nums[right];
+        int partition = left;
+        for(int i = left; i < right; i++){
+            if(_base > nums[i]){
+                (nums[partition],nums[i]) = (nums[i],nums[partition]);
+                partition ++; 
+            }           
         }
-        int pivot = nums[r];
-        int p = l;
-        for(int i = l; i < r; i++){
-            if(pivot > nums[i]){
-                (nums[p], nums[i]) = (nums[i], nums[p]);
-                p++;
-            }
+         (nums[partition],nums[right]) = (nums[right],nums[partition]);
+
+        if(partition > result_index){
+            return QuickSelect(nums,left, partition-1, result_index);
         }
-        (nums[p], nums[r]) = (nums[r], nums[p]);       
-        if(p > k){
-           return QuickSelect(nums, l,p-1, k);
-        }
-        else if(p < k){
-           return QuickSelect(nums, p +1, r, k);
+        else if(partition < result_index){
+            return QuickSelect(nums,partition+1, right, result_index);
         }
         else{
-            return nums[p];
+            return nums[partition];
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // private int QuickSelect(int[] nums, int l,int r,int k){
+    //     if(l == r){
+    //         return nums[l];
+    //     }
+    //     int pivot = nums[r];
+    //     int p = l;
+    //     for(int i = l; i < r; i++){
+    //         if(pivot > nums[i]){
+    //             (nums[p], nums[i]) = (nums[i], nums[p]);
+    //             p++;
+    //         }
+    //     }
+    //     (nums[p], nums[r]) = (nums[r], nums[p]);       
+    //     if(p > k){
+    //        return QuickSelect(nums, l,p-1, k);
+    //     }
+    //     else if(p < k){
+    //        return QuickSelect(nums, p +1, r, k);
+    //     }
+    //     else{
+    //         return nums[p];
+    //     }
+    // }
