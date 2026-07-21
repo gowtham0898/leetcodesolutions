5dfc9a2844f2968d5d3d24d61fbbdfcfecde9f3c
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-21 23:14:41 +0530

    Time: 91 ms (44.95%) | Memory: 67.2 MB (77.13%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cs
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cs
@@ -1,22 +1,22 @@
 public class Solution {
 
     public int[][] KClosest(int[][] points, int k) {
-        PriorityQueue<int[],int> min_heap = new();
+        PriorityQueue<int[],int> max_heap = new();
         foreach (var point in points){
                 int x = point[0];
                 int y = point[1];
             int sum = x*x + y*y;
-             min_heap.Enqueue(point,-sum);  
-             if(min_heap.Count > k){
-                min_heap.Dequeue();
+             max_heap.Enqueue(point,-sum);  
+             if(max_heap.Count > k){
+                max_heap.Dequeue();
              } 
         }
         int[][] res = new int[k][];
        int i = 0;
 
-        while(min_heap.Count >  0)
+        while(max_heap.Count >  0)
         {
-            res[i] = min_heap.Dequeue();
+            res[i] = max_heap.Dequeue();
             i++;
         }
         return res;
