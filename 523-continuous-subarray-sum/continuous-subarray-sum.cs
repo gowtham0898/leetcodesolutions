3a06984ad8272bf0b2b9eb091aabbac21734a86d
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-28 16:57:09 +0530

    Time: 14 ms (93.46%) | Memory: 84.6 MB (7.19%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cs
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cs
@@ -1,19 +1,20 @@
 public class Solution {
     public bool CheckSubarraySum(int[] nums, int k) {
-        Dictionary<int, int> map = new();
-        map[0] = -1;
+        Dictionary<int,int> result = new();
+        result[0] = -1;
         int pre_sum = 0;
-        for(int i =0; i< nums.Length; i++){
+        for(int i = 0; i < nums.Length; i++){
             pre_sum += nums[i];
-            int rem = pre_sum % k;
-            if(map.ContainsKey(rem)){
-                if(i - map[rem] > 1){
+
+            int mod = pre_sum % k;
+            if(result.ContainsKey(mod)){
+                if(i - result[mod] > 1){
                     return true;
                 }
+            }else{
+                 result[mod] = i;
             }
-            else{
-                map[rem] = i;
-            }
+           
         }
         return false;
     }
