1fc9c93968bc003cec2af20d3b18c39f1be91032
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-28 15:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (61.18%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cs
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cs
@@ -1,9 +1,12 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
-        int pre_sum = 0;
-        for(int i =0 ; i < nums.Length; i++){
-            pre_sum += nums[i];
-            nums[i] = pre_sum;
+        int cur = 0;
+        int l = nums.Length;
+        int i = 0;
+        while(i < l){
+            cur += nums[i];
+            nums[i] = cur;
+            i++;
         }
         return nums;
     }
