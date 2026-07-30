1ea131ab29e04690c30f2dd89bfcea9b8c31ad97
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-30 17:33:21 +0530

    Time: 16 ms (60.15%) | Memory: 49.1 MB (85.44%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cs
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cs
@@ -10,59 +10,24 @@
  * }
  */
 public class Solution {
-      public ListNode MergeKLists(ListNode[] lists) {
-        if(lists == null || lists.Length ==0){
-            return null;
+    public ListNode MergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode, int> min_list = new();
+        foreach(var list in lists){
+            if(list != null){
+                min_list.Enqueue(list,list.val);
+            }
+            
         }
-        return Merge(lists, 0, lists.Length -1);   
-      }
-      private ListNode Merge(ListNode[] lists, int left, int right){
-        if(left == right){
-            return lists[left];
-        }
-        int m = left + (right - left)/2;
-        ListNode l1 = Merge(lists, left, m);
-        ListNode l2 = Merge(lists, m+1, right);
-
-        return MergeTwoList( l1,  l2);
-      }
-      private ListNode MergeTwoList(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-
-        while( l1 != null && l2 != null){
-            if(l1.val < l2.val ){
-            tail.next = l1;
-            tail = tail.next;
-            l1 = l1.next;
-            }
-            else{
-                tail.next = l2;
-                tail = tail.next;
-                l2 = l2.next;
-            }
+        while(min_list.Count > 0){
+           var min_node = min_list.Dequeue();
+           tail.next = min_node;
+           tail = tail.next;
+           if(min_node.next != null){
+            min_list.Enqueue(min_node.next, min_node.next.val);
+           }
         }
-            tail.next = l1 ?? l2;
         return dummy.next;
-      }
-    // public ListNode MergeKLists(ListNode[] lists) {
-    //     PriorityQueue<ListNode,int> heap = new();
-
-    //     foreach(var list in lists){
-    //         if(list != null){
-    //             heap.Enqueue(list,list.val);
-    //         }
-    //     }
-    //     ListNode dummy = new ListNode(0);
-    //     ListNode tail = dummy;
-    //     while(heap.Count > 0){
-    //         ListNode smallest = heap.Dequeue();
-    //         tail.next = smallest;
-    //         tail = tail.next;
-    //         if(smallest.next != null){
-    //             heap.Enqueue(smallest.next, smallest.next.val);
-    //         }
-    //     }
-    //     return dummy.next;
-    // }
+    }
 }
