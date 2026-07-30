c363d3a19c3d55214d6b208bec33bffe63e4fb95
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-30 11:45:11 +0530

    Time: 179 ms (32.85%) | Memory: 61.9 MB (18.52%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
@@ -1,98 +1,14 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        PriorityQueue<int,int> min_heamp = new();
-        for(int i = 0; i < nums.Length; i++){
-            min_heamp.Enqueue(nums[i],nums[i]);
+       PriorityQueue<int,int> min_heap = new();
 
-            if(min_heamp.Count > k){
-                min_heamp.Dequeue();
-            }
+       for(int i = 0; i < nums.Length; i++){
+        min_heap.Enqueue(nums[i],nums[i]);
+        while(min_heap.Count > k){
+            min_heap.Dequeue();
         }
+       } 
 
-        return min_heamp.Dequeue();
-     }
-    //   public int FindKthLargest(int[] nums, int k) { 
-    //    PriorityQueue<int,int> max_heap = new();
-    //    for(int i = 0; i < nums.Length; i++){
-    //          max_heap.Enqueue(nums[i], -nums[i]);
-    //    }
-    //   int res = 0;
-    //   while(k > 0){
-    //     res = max_heap.Dequeue();
-    //     k--;
-    //   }
-    //   return res;
-    // }
-
-    // public int FindKthLargest(int[] nums, int k) { 
-    //     k = nums.Length - k;
-
-    //     return QuickSelect(nums, 0, nums.Length - 1, k);
-    // }
-
-    // private int QuickSelect(int[] nums, int left, int right, int result_index){
-    //     int _base = nums[right];
-    //     int partition = left;
-    //     for(int i = left; i < right; i++){
-    //         if(_base > nums[i]){
-    //             (nums[partition],nums[i]) = (nums[i],nums[partition]);
-    //             partition ++; 
-    //         }           
-    //     }
-    //      (nums[partition],nums[right]) = (nums[right],nums[partition]);
-
-    //     if(partition > result_index){
-    //         return QuickSelect(nums,left, partition-1, result_index);
-    //     }
-    //     else if(partition < result_index){
-    //         return QuickSelect(nums,partition+1, right, result_index);
-    //     }
-    //     else{
-    //         return nums[partition];
-    //     }
-
-    // }
+       return min_heap.Dequeue();
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // private int QuickSelect(int[] nums, int l,int r,int k){
-    //     if(l == r){
-    //         return nums[l];
-    //     }
-    //     int pivot = nums[r];
-    //     int p = l;
-    //     for(int i = l; i < r; i++){
-    //         if(pivot > nums[i]){
-    //             (nums[p], nums[i]) = (nums[i], nums[p]);
-    //             p++;
-    //         }
-    //     }
-    //     (nums[p], nums[r]) = (nums[r], nums[p]);       
-    //     if(p > k){
-    //        return QuickSelect(nums, l,p-1, k);
-    //     }
-    //     else if(p < k){
-    //        return QuickSelect(nums, p +1, r, k);
-    //     }
-    //     else{
-    //         return nums[p];
-    //     }
-    // }
