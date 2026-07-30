0d05b3fd37e9d237a05a51fa1945411985b1a841
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-30 16:06:21 +0530

    Time: 2383 ms (5.07%) | Memory: 59.1 MB (77.84%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cs
@@ -1,14 +1,46 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-       PriorityQueue<int,int> min_heap = new();
+        k = nums.Length - k;
+       int l = 0;
+       int  r = nums.Length -1;
 
-       for(int i = 0; i < nums.Length; i++){
-        min_heap.Enqueue(nums[i],nums[i]);
-        if(min_heap.Count > k){
-            min_heap.Dequeue();
-        }
-       } 
+      return  QuickSearch(nums,l,r,k);
 
-       return min_heap.Peek();
     }
+
+    private int QuickSearch(int[] nums, int l, int r, int k){
+       if( l ==  r){
+        return nums[l];
+       }
+        int p = l;
+
+        for(int i = l ; i < r; i++){
+            if(nums[i] < nums[r]){
+                (nums[i],nums[p]) = (nums[p],nums[i]);
+                p++;
+            }
+       }
+       (nums[p],nums[r]) = (nums[r],nums[p]);
+
+        if(k < p){
+           return QuickSearch(nums,l,p-1,k);
+        }
+        else if(k > p){
+           return QuickSearch(nums,p+1,r,k);
+        }
+        else{
+            return nums[k];
+        }
+
+    }
+    //    PriorityQueue<int,int> min_heap = new();
+
+    //    for(int i = 0; i < nums.Length; i++){
+    //     min_heap.Enqueue(nums[i],nums[i]);
+    //     if(min_heap.Count > k){
+    //         min_heap.Dequeue();
+    //     }
+    //    } 
+
+    //    return min_heap.Peek();
 }
