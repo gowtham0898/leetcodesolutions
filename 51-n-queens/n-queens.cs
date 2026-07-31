f8c1e8b6280f20792db9fe4de50f3c4ebcdd8100
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-31 16:24:26 +0530

    Time: 10 ms (65.13%) | Memory: 53.1 MB (92.44%) - LeetSync

--- 51-n-queens/n-queens.cs
+++ 51-n-queens/n-queens.cs
@@ -1,56 +1,59 @@
 public class Solution {
     public IList<IList<string>> SolveNQueens(int n) {
-        char[][] board = new char [n][];
-        for(int i =0 ; i < n; i ++){
-            board[i] =new char[n]; 
-            Array.Fill(board[i], '.');
+        char[][] board = new char[n][];
+        for(int i =0; i < n; i++){
+           board[i] = new char[n];
+           Array.Fill(board[i], '.');
         }
-    IList<IList<string>> result = new List<IList<string>>();
-      
-            nQueen(result, 0, n,board);
-        return result;
+         IList<IList<string>> result = new List<IList<string>>();
+           
+                solve(board,0,n,result);
+            return result;
     }
-
-    private void nQueen(IList<IList<string>> result, int c, int n, char[][] board){
-
+    private void solve(char[][] board,int c, int n, IList<IList<string>> result){
         if(c == n){
             List<string> temp = new();
-            foreach(var ro in board){
-                temp.Add(new string(ro));
+            foreach(var res in board){
+                temp.Add(new string(res));
             }
             result.Add(temp);
             return;
         }
-        for(int r = 0; r < n; r++){
-            if(isValid(c,r,board,n)){
-                board[r][c] = 'Q';
-                nQueen(result, c+1,n,board);
-                board[r][c] = '.';
+        for(int r = 0; r<n;r++){
+            if(isValid(r,c,n, board)){
+               board[r][c]  = 'Q';
+               solve(board,c+1,n,result);
+                 board[r][c]  = '.';
             }
         }
     }
 
- private bool isValid(int c, int r, char[][] board, int n){
-    int copy_r = r;
-    int copy_c = c;
-
-    while(r >= 0 &&  c >= 0){
-        if( board[r][c] == 'Q') return false;
-        c--; r--;
-    } 
-     r = copy_r;
-    c = copy_c;
-    while(c  >= 0 ){
-       if( board[r][c] == 'Q') return false;
-           c--;
+    private bool isValid(int r, int c, int n, char[][] board){
+        int copy_c = c;
+        int copy_r = r;
+        while(r >= 0 && c >= 0){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+            r--;c--;
+        }
+        c = copy_c;
+        r = copy_r;
+        while(c >= 0){
+             if(board[r][c] == 'Q'){
+                return false;
+            }
+            c--;
+        }
+        c = copy_c;
+        r = copy_r;
+        while(r < n &&  c >= 0){
+             if(board[r][c] == 'Q'){
+                return false;
+            }
+           r++;c--;
+        }
+        return true;
     }
-   r = copy_r;
-    c = copy_c;
-    while ( c >= 0 && r < n){
-         if( board[r][c] == 'Q') return false;
-         c--; r++;
-    }
 
-    return true;
- }
 }
