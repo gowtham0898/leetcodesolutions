2c20c7bc04a477508d2d09b59f8c6ac5805fc98b
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-07-31 12:06:52 +0530

    Time: 1421 ms (15.38%) | Memory: 43.4 MB (61.00%) - LeetSync

--- 79-word-search/word-search.cs
+++ 79-word-search/word-search.cs
@@ -1,41 +1,28 @@
 public class Solution {
     public bool Exist(char[][] board, string word) {
-         int row = board.Length;
-         int col = board[0].Length;
-         HashSet<(int, int)> used = new();
-
-         for(int r= 0; r < row; r++){
-            for(int c = 0; c < col; c++){
-                if(Match(board, word,r,c,0,used)){
-                    return true;
-                }
+        HashSet<(int,int)> occur = new();
+        for(int r = 0 ; r < board.Length; r++){
+            for(int c = 0 ; c < board[0].Length; c++){
+                if(Solve(r,c,0,board,word,occur)) return true;
             }
-         } 
-        return false;   
-    }
-    private bool Match(char[][] board, string word,int r, int c, int index, HashSet<(int,int)> used){
-        if(index == word.Length) return true;
-
-        if(r < 0 || c < 0 || r >= board.Length || c >= board[0].Length){
-                return false;
-        }   
-        // current letter no matching with result index
-        if(board[r][c] != word[index]){
-            return false;
         }
-        // alreadu used
-        if(used.Contains((r,c))) return false;
-
-        // add
-        used.Add((r,c));
-
-        // track
-        bool found = Match(board, word,r +1,c,index +1 ,used) ||
-                     Match(board, word,r -1,c,index +1 ,used) ||  
-                     Match(board, word,r ,c +1,index +1 ,used) || 
-                     Match(board, word,r,c -1,index +1 ,used);
-
-        used.Remove((r,c));  
-        return found;                   
+        return false;
+    }
+    private bool Solve(int r, int c, int i,char[][] board, string word, HashSet<(int,int)> occur){
+        int row = board.Length -1; int col = board[0].Length -1;
+        if(i == word.Length){
+            return true;
+        }        
+        if( r < 0 || c < 0 || r > row || c >col||
+            word[i] != board[r][c] || occur.Contains((r,c))){
+                return false;
+            }
+         occur.Add((r,c));
+         var res = (Solve(r +1,c,i+1,board,word,occur)||
+                    Solve(r -1,c,i+1,board,word,occur)||
+                    Solve(r,c+1,i+1,board,word,occur)||
+                    Solve(r,c-1,i+1,board,word,occur));
+                    occur.Remove((r,c));
+            return res;   
     }
 }
