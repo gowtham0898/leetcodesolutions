f3125cf60ae66109db6e671e6715659b2490de80
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-08-11 16:17:57 +0530

    Time: 171 ms (5.12%) | Memory: 53.3 MB (33.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.cs
+++ 200-number-of-islands/number-of-islands.cs
@@ -1,60 +1,28 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        HashSet<(int,int)> conatins = new();
-        int row = grid.Length;
-        int col = grid[0].Length;
-        int island = 0;
-        for(int r = 0; r < row; r++){
-            for(int c = 0; c < col; c++){
-                if(grid[r][c] == '1' && !conatins.Contains((r,c))){
-                    BFS(r,c,conatins,grid);
-                    island ++;
+        int is_land= 0;
+         HashSet<(int,int)> set = new();
+        for(int r = 0; r < grid.Length; r++){
+            for(int c = 0; c < grid[0].Length; c++){
+                if(grid[r][c] =='1' && !set.Contains((r,c))){
+                         DFS(r,c,grid,set);
+                     is_land++;    
                 }
+               
             }
         }
-        return island;
+        return is_land;
     }
 
-    private void BFS(int r1, int c1,HashSet<(int,int)> conatins, char[][] grid){
-        Queue<(int,int)> queue = new();
-         int[][] directions  = {
-            new[] {-1,0},
-            new[] {0,1},
-            new[] {1,0},
-            new[] {0,-1}
-        };
-        queue.Enqueue((r1,c1));
-        conatins.Add((r1,c1));
-        while(queue.Count >0){
-        var (r,c) = queue.Dequeue();       
-        foreach(var dr in directions){
-            int new_row = r + dr[0];
-            int new_col = c + dr[1];
-            if(new_row >= 0 && new_row < grid.Length && new_col >= 0 &&
-             new_col < grid[0].Length && !conatins.Contains((new_row,new_col)) &&
-             grid[new_row][new_col] == '1'){
-                  conatins.Add((new_row,new_col));
-                  queue.Enqueue((new_row,new_col));
-             }
-            }
-        }
-        // if(r > 0 && !conatins.Contains((r-1,c)) && grid[r -1][c] == '1'){
-        // conatins.Add((r-1,c));
-        // queue.Enqueue(((r-1,c)));
-        // }
-        // if(c < grid[0].Length -1 && !conatins.Contains((r,c+1))&& grid[r][c+1] == '1'){
-        //     conatins.Add((r,c+1));
-        //     queue.Enqueue(((r,c+1)));
-        // }
-        // if(r < grid.Length -1 && !conatins.Contains((r+1,c)) && grid[r +1][c] == '1'){
-        //     conatins.Add((r+1,c));
-        //     queue.Enqueue(((r+1,c)));
-        // }
-        // if(c > 0 && !conatins.Contains((r,c-1)) && grid[r][c-1] == '1'){
-        //     conatins.Add((r,c-1));
-        //     queue.Enqueue(((r,c-1)));
-        // }
-        // }
-                
+    public void DFS(int r, int c, char[][] grid, HashSet<(int,int)> set){
+        if(r < 0 || r >= grid.Length || c < 0 || c >= grid[0].Length ||
+           set.Contains((r,c)) || grid[r][c] =='0'){
+            return ;
+           }
+           set.Add((r,c));
+                     DFS(r+1,c,grid,set);
+                    DFS(r-1,c,grid,set);
+                    DFS(r,c+1,grid,set);
+                    DFS(r,c-1,grid,set);                       
     }
 }
