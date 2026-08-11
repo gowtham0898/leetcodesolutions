b0a27499f73ee947fe12374e5317fe2b3f56180d
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-08-11 16:55:27 +0530

    Time: 0 ms (100.00%) | Memory: 49.5 MB (57.01%) - LeetSync

--- 733-flood-fill/flood-fill.cs
+++ 733-flood-fill/flood-fill.cs
@@ -1,40 +1,18 @@
 public class Solution {
     public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
-        
-        if(image.Length ==0){
-            return image;
-        }
 
-        HashSet<(int,int)> contain = new();
-        Queue<(int,int)> queue = new();
-        int row = image.Length;
-        int col = image[0].Length;
-        contain.Add((sr,sc));
-        queue.Enqueue((sr,sc));    
-        int original = image[sr][sc];
-        if(original == color){
-            return image;
-        }
-        image[sr][sc] = color;
-        int[][] directions = {
-            new[] {-1,0},
-            new[] {0,1},
-            new[] {1,0},
-            new[] {0,-1}
-        };
-        while (queue.Count > 0){
-            var (r,c) = queue.Dequeue();
-            foreach(var dir in directions){
-                int new_row = r + dir[0];
-                int new_col = c + dir[1];
-                if(new_row >= 0 && new_row < row && new_col >= 0 && new_col < col
-                 && !contain.Contains((new_row,new_col)) && image[new_row][new_col]== original){
-                     contain.Add((new_row,new_col));
-                    queue.Enqueue((new_row,new_col));   
-                    image[new_row][new_col] = color;
-                }                
-            }
-        }
-    return image;
+          DFS(image, sr,sc,color, image[sr][sc]);
+          return image;
+    }
+    private void DFS(int[][] image, int sr, int sc, int color, int original_color){
+        if(sr < 0 || sr >= image.Length|| sc< 0 || sc >= image[0].Length ||
+           image[sr][sc] == color || original_color != image[sr][sc]){
+            return;
+           }
+           image[sr][sc] = color;
+           DFS(image, sr+1,sc,color,original_color);
+           DFS(image, sr-1,sc,color,original_color);
+           DFS(image, sr,sc+1,color,original_color);
+           DFS(image, sr,sc-1,color,original_color);
     }
 }
