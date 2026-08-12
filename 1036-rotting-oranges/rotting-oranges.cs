510768607a7b215f1e833f4a93df53fee3576b1b
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-08-12 07:07:56 +0530

    Time: 2 ms (78.49%) | Memory: 42.8 MB (41.05%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cs
+++ 1036-rotting-oranges/rotting-oranges.cs
@@ -1,39 +1,44 @@
 public class Solution {
     public int OrangesRotting(int[][] grid) {
-        Queue<(int,int)> queue = new();
-        int fresh = 0;
         int time = 0;
+        int fresh = 0;
+        Queue<(int,int)> queue = new();
         for(int r = 0; r < grid.Length; r++){
             for(int c = 0; c < grid[0].Length; c++){
-                if(grid[r][c] == 1) fresh++;
-                 if(grid[r][c] == 2) queue.Enqueue((r,c));
+                if(grid[r][c] == 1){
+                    fresh++;
+                }
+                if(grid[r][c]== 2){
+                    queue.Enqueue((r,c));
+                }
             }
         }
         int[][] directions = {
             new[] {0,1},
             new[] {0,-1},
             new[] {1,0},
-            new[] {-1,0}
+            new[] {-1,0},
         };
-        while(queue.Count > 0 && fresh > 0){
-            int q_count  = queue.Count;
+        while(queue.Count > 0 && fresh >0){
+            int q_count = queue.Count;
             for(int i = 0; i < q_count; i++){
-                var (r,c) = queue.Dequeue();
+                var(r,c) = queue.Dequeue();
                 foreach(var dir in directions){
-                    int new_row = r + dir[0];
+                    int new_row = r+dir[0];
                     int new_col = c + dir[1];
-                    if(new_row >= 0 && new_row < grid.Length &&
-                       new_col >= 0 && new_col < grid[0].Length && 
-                       grid[new_row][new_col] == 1)
-                       {
-                        grid[new_row][new_col] = 2;
-                        fresh--;
-                        queue.Enqueue((new_row,new_col));
+                    if(new_row < 0 || new_row >= grid.Length||
+                       new_col < 0 || new_col >= grid[0].Length|| 
+                       grid[new_row][new_col] == 0 ||grid[new_row][new_col] ==2 ){
+                        continue;
                        }
+                       queue.Enqueue((new_row,new_col));
+                       grid[new_row][new_col] = 2;
+                       fresh--;
                 }
             }
             time++;
+
         }
-        return fresh ==0 ? time:-1;
+        return fresh == 0? time:-1;
     }
 }
