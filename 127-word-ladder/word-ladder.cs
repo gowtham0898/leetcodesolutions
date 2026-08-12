49e72a8798248ff228e97d1d3efd87ae26663310
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-08-12 07:49:03 +0530

    Time: 74 ms (70.47%) | Memory: 70 MB (8.19%) - LeetSync

--- 127-word-ladder/word-ladder.cs
+++ 127-word-ladder/word-ladder.cs
@@ -1,46 +1,42 @@
 public class Solution {
     public int LadderLength(string beginWord, string endWord, IList<string> wordList) {
-
-        if(!wordList.Contains(endWord)){
-            return 0;
-        }
-        Dictionary<string,List<string>> map = new();
-        Queue<string> queue = new();
-        HashSet<string> visited = new();
-        wordList.Add(beginWord);
-        visited.Add(beginWord);
-        queue.Enqueue(beginWord);
-
-        foreach(var word in wordList){
-            int count = word.Length;
-            for(int i = 0; i < count; i++){
-               string pattern = word.Substring(0,i)+'*'+word.Substring(i+1);
-                if(!map.ContainsKey(pattern)){
-                    map[pattern] =  new List<string>();
-                }
-                map[pattern].Add(word);
-            }
-        }
-        int item = 1;
-        while(queue.Count > 0){
-            int q_count = queue.Count;
-            for(int i = 0; i < q_count; i++){
-                var d_que = queue.Dequeue();
-
-                if(d_que == endWord) return item;
-
-                for(int j = 0; j < d_que.Length; j++){
-                    string re_pat = d_que.Substring(0,j)+'*'+d_que.Substring(j+1);
-                    foreach(var wr in map[re_pat]){
-                        if(!visited.Contains(wr)){
-                            visited.Add(wr);
-                            queue.Enqueue(wr);                            
-                        }
-                    }
-                }
-            }
-            item++;
-        }
+       if(!wordList.Contains(endWord)){
         return 0;
+       } 
+       Dictionary<string, List<string>> map = new();
+       wordList.Add(beginWord);
+       foreach( var word in wordList){
+        for(int i = 0; i < word.Length; i++){
+            var ptrn = word.Substring(0,i) +'*' + word.Substring(i+1);
+            if(!map.ContainsKey(ptrn)){
+                map[ptrn] = new List<string>();
+            }
+            map[ptrn].Add(word);
+         }
+       }
+       Queue<string> que = new();
+       HashSet<string> visit = new();
+       que.Enqueue(beginWord);
+       visit.Add(beginWord);
+        int level = 1;
+       while(que.Count > 0){
+        int qu_c = que.Count;
+        for(int c = 0; c< qu_c; c++){
+            var wrd = que.Dequeue();
+            if(wrd == endWord) return level;
+            for(int i = 0; i < wrd.Length; i++){
+                string ptr = wrd.Substring(0,i)+'*'+ wrd.Substring(i+1);
+                foreach(var wr in map[ptr]){
+                    if(visit.Contains(wr)){
+                        continue;
+                    }
+                    visit.Add(wr);
+                    que.Enqueue(wr);
+                }
+            }
+        }
+        level ++;
+       }
+       return 0;
     }
 }
