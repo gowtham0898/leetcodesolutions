dc1fa3ea06b07f0bfb2e36546dd1d8526d1d05e6
gowtham0898 <115063182+gowtham0898@users.noreply.github.com>
2026-08-17 14:28:17 +0530

    Time: 15 ms (22.86%) | Memory: 48.8 MB (16.76%) - LeetSync

--- 383-ransom-note/ransom-note.cs
+++ 383-ransom-note/ransom-note.cs
@@ -1,42 +1,21 @@
 public class Solution {
     public bool CanConstruct(string ransomNote, string magazine) {
-        if(ransomNote.Length > magazine.Length){
-            return false;
+        Dictionary<char,int> map = new();
+        foreach(char mag in magazine){
+            if(!map.ContainsKey(mag)){
+              map[mag] = 0;      
+            }
+            map[mag]++;
         }
-        int[] num = new int[26];
 
-        for(int i =0; i<magazine.Length;i++){
-            num[magazine[i] - 'a'] ++;
-        }
-        for(int i = 0; i<ransomNote.Length; i++){
-             num[ransomNote[i] - 'a'] --;
-             if(num[ransomNote[i] - 'a'] < 0){
-                return false;
-             }
+        foreach(char rn in ransomNote){
+            if(!map.ContainsKey(rn)) return false;
+
+            map[rn]--;
+            if(map[rn] == 0){
+                map.Remove(rn);
+            }
         }
         return true;
-        // Dictionary<char, int> map = new();
-
-        // for(int i =0; i<magazine.Length;i++){
-        //     if(map.ContainsKey(magazine[i])){
-        //        map[magazine[i]]++;
-        //     }
-        //     else{
-        //         map[magazine[i]] = 1;
-        //     }
-        // }   
-
-        // for(int i = 0; i<ransomNote.Length; i++){
-        //     if(map.ContainsKey(ransomNote[i])){
-        //         map[ransomNote[i]]--;
-        //         if(map[ransomNote[i]] == 0){
-        //             map.Remove(ransomNote[i]);
-        //         }                
-        //     }
-        //     else{
-        //             return false;
-        //         }
-        // }
-        // return true;
     }
 }
